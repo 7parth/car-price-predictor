1359e88bedeb085d4ce3cf6074b9ac2c941abad5
7parth <waradkarparth@gmail.com>
2025-07-02 14:06:30 +0530

    added route for predict function

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import pandas as pd
+import pickle
 app = Flask(__name__)
 car = pd.read_csv('cleaned car.csv')
 
+model = pickle.load(open('LinearRegression.pkl','rb'))
 
 @app.route('/')
 def index():
@@ -14,8 +16,17 @@ def index():
     
     return render_template('index.html',companies = companies, car_models = car_models , years = year , fuel_types = fuel_types)
 
-@app.route('predict',methods=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
-    return
+    company = request.form.get('company')
+    car_model = request.form.get('car_model')
+    year= int(request.form.get('year'))
+    fuel_types = request.form.get('fuel_types')
+    kms_driven = request.form.get('kilo_driven')
+    print(company,car_model,year,fuel_types,kms_driven)
+    
+    return ""
+
+
 if __name__ == "__main__":
     app.run(debug=True)
