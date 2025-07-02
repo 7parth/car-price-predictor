bf05a7f9af98450a9af74d87bdfc4b1a4d1fce4b
7parth <waradkarparth@gmail.com>
2025-07-02 13:56:58 +0530

    added script for backend interactions

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ def index():
     companies = sorted(car['company'].unique(),reverse=True)
     car_models = sorted(car['name'].unique(),reverse=True)
     year = sorted(car['year'].unique(),reverse=True)
-    fuel_type = sorted(car['fuel_type'].unique(),reverse=True)
+    fuel_types = sorted(car['fuel_type'].unique(),reverse=True)
     
     
-    return render_template('index.html',companies = companies, car_models = car_models , year = year , fuel_type = fuel_type)
+    return render_template('index.html',companies = companies, car_models = car_models , years = year , fuel_types = fuel_types)
 
+@app.route('predict',methods=['POST'])
+def predict():
+    return
 if __name__ == "__main__":
     app.run(debug=True)
